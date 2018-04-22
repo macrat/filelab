f87f254371282adf02e1256bf11130dacba18f12
MacRat <m@crat.jp>
2018-04-22 19:10:17 +0900

    Remade API to using WebDAV

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,31 @@
 		<h1>{{ path }}</h1>
 		<ul>
 			<li v-for="file in files">
-				<nuxt-link :to=file.path :type=file.type v-if="file.type === 'text/directory'">{{ file.path }} ({{ file.type }})</nuxt-link>
-				<a :href=file.endpoint :type=file.type target=_blank v-else>{{ file.path }} ({{ file.type }})</a>
+				<nuxt-link :to=file.filename v-if="file.type === 'directory'">{{ file.basename }}</nuxt-link>
+				<a :href=file.downloadLink :type=file.mime target=_blank v-else>{{ file.basename }} ({{ file.mime }})</a>
 			</li>
 		</ul>
 	</div>
 </template>
 
 <script>
-import axios from 'axios';
+import mime from 'mime';
+import webdavClient from 'webdav';
 
 
 export default {
 	async asyncData({params}) {
+		const client = webdavClient('http://localhost:3001');
+
 		const path = '/' + (params.path || '');
 		let files = [];
 		let exists = true;
 
 		try {
-			files = (await axios.get('http://localhost:3000/api' + path)).data.contents;
+			files = (await client.getDirectoryContents(path)).map(x => Object.assign(x, {
+				mime: x.type === 'directory' ? 'text/directory' : (x.mime || mime.getType(x.basename) || 'application/octet-stream'),
+				downloadLink: x.type === 'directory' ? '' : client.getFileDownloadLink(x.filename),
+			}));
 		} catch(e) {
 			exists = false;
 		}
